4ae6157793847b030ece82e9d73c499f2fda46ec
bessamsirisha <bessamsirisha251@gmail.com>
2025-01-30 18:11:05 +0530

    Add files via upload

---
+++ Functions.java
@@ -0,0 +1,17 @@
+public class Functions {
+    public static void main(String[] args) {
+        System.out.println("Hai,");
+        //Greet.msg();
+        Greet.mess.Gt();
+    }
+}
+class Greet{
+    static void msg(){
+        System.out.println("Hello,");
+    }
+    public class  mess{
+        public static void Gt(){
+            System.out.println("welcome.");
+        }
+    }
+}
